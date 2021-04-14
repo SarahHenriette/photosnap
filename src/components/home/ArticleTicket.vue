e2a3febfbfa4de8a0f29c6005e055d64c36df505
elise <rousseaujumelle@gmail.com>
2021-04-14 17:44:26 +0200

    update menu burger and integration article ticket homepage

---
+++ src/components/home/ArticleTicket.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="articleTicket" :class="classNameArticleTicket">
+        <div class="articleTicket--text">
+            <div class="articleTicket--text_contain">
+                <h2 class="h2-large"><slot name="title"></slot></h2>
+                <p><slot name="paragraphe"></slot></p>
+                <router-link to="/stories" class="btn-2"><slot name="button"></slot><svg width="42" height="14" viewBox="0 0 42 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 7H41.864" :stroke="color"/><path d="M35.4282 1L41.4282 7L35.4282 13" :stroke="color"/></svg></router-link>
+            </div>
+        </div>
+        <div class="articleTicket--image" :class="className"></div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ["color", "className", "classNameArticleTicket"]
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,43 @@
 <template>
-    <h1 class="home">HOME</h1>
+    <div>
+      <ArticleTicket color="white" className="articleTicket--image_createAndShare" 
+      classNameArticleTicket="articleTicket--principal">
+        <template v-slot:title>Create and share your photo stories.</template>
+        <template v-slot:paragraphe>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</template>
+        <template v-slot:button>get an invite</template>
+      </ArticleTicket>
+      <ArticleTicket color="black" className="articleTicket--image_beautifulStories" 
+      classNameArticleTicket="articleTicket--secondary">
+        <template v-slot:title>Beautiful stories every time</template>
+        <template v-slot:paragraphe>We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.</template>
+        <template v-slot:button>view the stories</template>
+      </ArticleTicket>
+      <ArticleTicket color="black" className="articleTicket--image_designedForEveryone" 
+      classNameArticleTicket="articleTicket--secondary">
+        <template v-slot:title>Designed for everyone</template>
+        <template v-slot:paragraphe>Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.</template>
+        <template v-slot:button>view the stories</template>
+      </ArticleTicket>
+ 
+    </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
+import ArticleTicket from "./../components/home/ArticleTicket"
 
 export default {
   name: 'Home',
+
   components: {
+    ArticleTicket
   }
 }
 </script>
 
 <style lang="scss" scoped>
  .home{
-        height: 800px;
-        width: 80%;
-        background-color: rgba(66, 3, 3, 0.842);
-        margin: auto;
-    }
+
+  }
 </style>
