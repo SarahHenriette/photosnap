6274e7d9805942ad9818ca4c19ffc2143a536ccb
elise <rousseaujumelle@gmail.com>
2022-03-26 17:45:15 +0100

    refacto

--- src/components/home/Breakpoint.vue
+++ src/components/home/Breakpoint.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="breakpoint">
         <slot name="iconBreakpoint"></slot>
-        <section role="paragraph" class="breakpoint--text">
+        <div role="contentText" class="breakpoint--text">
             <h3 class="breakpoint--text_nameBreakpoint">{{ nameBreakpoint }}</h3>
             <p>{{ text }}</p>
-        </section>
+        </div>
     </section>
 </template>
 
